a90175c6818f0e96936e9f0fd5062df1c224ed51
ayden matache <acmat668@learner.dvusd.org>
2024-01-08 09:30:42 -0700

    mondate2
    code

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number.");
+            int num=0;
+            int.TryParse(Console.ReadLine(),out num);
+            Console.WriteLine(num * 2);
+            Console.ReadLine();
+        }
+    }
+}
